bcb7657e484fd7e796f7296e1778d67edc011098
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2016-02-02 08:01:04 +0000

    Modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
-ln -s ~/dotfiles/custom.aliases.bash ~/.bash_it/aliases/
+source ~/.bashrc
 
+# enable settings
+#bash-it enable aliase git
+bash-it enable completion git ssh
+#bash-it enable plugin git
+
+# custom settings for bash
+ln -s ~/dotfiles/custom.aliases.bash ~/.bash_it/aliases/
+ln -s ~/dotfiles/custom.completion.bash ~/.bash_it/completion/
+ln -s ~/dotfiles/custom.plugin.bash ~/.bash_it/plugins/
+
+# dotfiles
 DOT_FILES=(.gitconfig .gitignore .inputrc)
 for file in ${DOT_FILES[@]}
 do
     ln -s ~/dotfiles/$file ~/$file
 done
-
-source ~/.bashrc
-
-bash-it enable completion git ssh
-
-echo "" >> ~/.bashrc
-echo "source ~/dotfiles/custom.completion.bash" >> ~/.bashrc
-echo "source ~/dotfiles/custom.option.bash" >> ~/.bashrc
 
 exec $SHELL -l
 
