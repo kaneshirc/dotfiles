20e49f612f041c5fc01613b8ddfdcfeb4b29af4b
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-02-02 07:19:21 +0000

    add bash settings

---
+++ custom.option.bash
@@ -0,0 +1,6 @@
+# パス名展開で「**」を使えるようにする - globstar
+shopt -s globstar
+# cdコマンドを省略する - autocd
+shopt -s autocd
+# 大文字小文字無視パス名展開 - nocaseglob
+shopt -s nocaseglob
--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 
 ln -s ~/dotfiles/custom.aliases.bash ~/.bash_it/aliases/
 ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.inputrc ~/
 
 source ~/.bash_it/bash_it.sh
 
 echo "" >> ~/.bashrc
 echo "source ~/dotfiles/custom.completion.bash" >> ~/.bashrc
+echo "source ~/dotfiles/custom.option.bash" >> ~/.bashrc
 
 exec $SHELL -l
 
