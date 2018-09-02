cd5b4d9beb5141f84237d242733dca632ae9cd03
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2018-09-02 11:04:47 +0900

    無駄な設定削除、.bashrc.localを使うようにした

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
-source ~/.bashrc
-
-# enable settings
-#bash-it enable aliase git
-bash-it enable completion git ssh
-#bash-it enable plugin git
-
 # custom settings for bash
 ln -s ~/dotfiles/custom.aliases.bash ~/.bash_it/aliases/
 ln -s ~/dotfiles/custom.completion.bash ~/.bash_it/completion/
@@ -19,6 +12,9 @@ for file in ${DOT_FILES[@]}
 do
     ln -s ~/dotfiles/$file ~/$file
 done
+
+touch ~/.bashrc.local
+echo "source \"\$HOME\"/.bashrc.local" >> ~/.bashrc
 
 source ~/.bashrc
 
