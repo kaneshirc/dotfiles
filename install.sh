f426471fb7cf5d5747151df790ad4caac3d392c9
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2018-09-03 19:07:45 +0900

    ctrl+f ctrf+b で単語移動できるようにした

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
 # custom settings for bash
+ln -s ~/dotfiles/custom.bash ~/.bash_it/custom/
 ln -s ~/dotfiles/custom.aliases.bash ~/.bash_it/aliases/
 ln -s ~/dotfiles/custom.completion.bash ~/.bash_it/completion/
 ln -s ~/dotfiles/custom.plugins.bash ~/.bash_it/plugins/
