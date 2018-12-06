830f9c4502631016090cdc6711e7887a7209aa6f
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2018-12-06 16:49:47 +0900

    add alias

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -4,3 +4,11 @@ alias d="docker"
 alias dc="docker-compose"
 alias be="bundle exec"
 
+case $OSTYPE in
+  darwin*)
+    alias brew='PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin brew'
+    ;;
+  *)
+    ;;
+esac
+
