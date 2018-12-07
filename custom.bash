8d7200afd8984aec671800b902c64d170d399a68
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2018-12-07 13:56:38 +0900

    add virtualenv and google-cloud-sdk settings.

--- custom.bash
+++ custom.bash
@@ -1,3 +1,10 @@
 bind '"\C-f": forward-word'
 bind '"\C-b": backward-word'
 
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+GOOGLE_CLOUD_SDK='/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk'
+if [ -e $GOOGLE_CLOUD_SDK ]; then
+  source $GOOGLE_CLOUD_SDK/path.bash.inc
+  source $GOOGLE_CLOUD_SDK/completion.bash.inc
+fi
