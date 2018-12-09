48d141aadceed3e39eb13a2115ea8585bba7e45c
Naoki Kaneshiro <kaneshiro@rightcode.co.jp>
2018-12-09 23:23:24 +0900

    pipenvの設定を追加

--- custom.bash
+++ custom.bash
@@ -3,6 +3,8 @@ bind '"\C-b": backward-word'
 
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
+export PIPENV_VENV_IN_PROJECT=true
+
 GOOGLE_CLOUD_SDK='/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk'
 if [ -e $GOOGLE_CLOUD_SDK ]; then
   source $GOOGLE_CLOUD_SDK/path.bash.inc
